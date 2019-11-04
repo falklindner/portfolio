9802618f3f1b30d804a7220650cefc6d05c9d0b2
falklindner <lindner.falk@googlemail.com>
2019-11-05 00:41:00 +0100

    csv import final

--- input.py
+++ input.py
@@ -4,7 +4,7 @@ import datetime
 import matplotlib.pyplot as plt
 
 headers = ["Execute", "Booking", "No", "Name", "WKN", "Currency", "Price", "Trans"]
-
+datefrmt = lambda x: pd.datetime.strptime(x, "%d.%m.%Y")
 amount = pd.read_csv("ums.csv",
     skiprows = 4,
     delimiter = ";",
@@ -20,6 +20,7 @@ input = pd.read_csv("ums.csv",
     delimiter = ";",
     encoding = "cp1252",
     parse_dates = [0,1],
+    date_parser = datefrmt, 
     decimal = ",",
     usecols = [0,1,3,4,5,6,7],
     names = ["Execute","Booking","Name","WKN","Currency","Price","Trans"], 
@@ -27,3 +28,7 @@ input = pd.read_csv("ums.csv",
 )
 
 input = pd.concat([input,amount], axis = 1)
+input["Fees"] = round(input["Trans"]-(input["Amount"]*input["Price"]),2)
+input
+
+input.loc[(input["Execute"] < datetime.datetime(2019,2,1)) & (input["WKN"] == "ETF110")]
