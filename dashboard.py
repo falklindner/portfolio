a896d45001cab3d191bb77ecc58fd4cd25333780
falklindner <lindner.falk@googlemail.com>
2020-01-26 11:11:46 +0100

    Dash Visualization working

--- dashboard.py
+++ dashboard.py
@@ -1,33 +1,62 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import pandas as pd
+import plotly.graph_objects as go
+import PortfolioView
+
+portfolio = "Airbus"
+symbol = "AIR.PA"
+
+portfolio_view = PortfolioView.ReadPortfolioView()
+xdata = portfolio_view.index
+ydata = portfolio_view.loc[:,(portfolio,symbol,"XIRR")].values
+
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 12], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
+component1 = go.Scatter(
+    x=xdata,
+    y=ydata
     )
+
+
+
+
+layout = dict(
+    title='Time Series with Rangeslider',
+    xaxis=dict(
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1,
+                     label='1m',
+                     step='month',
+                     stepmode='backward'),
+                dict(count=6,
+                     label='6m',
+                     step='month',
+                     stepmode='backward'),
+                dict(step='all')
+            ])
+        ),
+        rangeslider=dict(),
+        type='date'
+    )
+)
+
+
+data =[component1]
+fig = dict(data=data, layout=layout)
+
+# Now here's the Dash part:
+
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.layout = html.Div([
+    dcc.Graph(id='my-graph', figure=fig)
 ])
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
